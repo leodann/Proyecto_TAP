93a63369a55d01b7b29aace0ed8180c7f07ccc40
AlejandroSaldanaContreras <36738279+AlejandroSaldanaContreras@users.noreply.github.com>
2018-04-13 10:11:49 -0500

    'Escenas log, sign y main'

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,11 @@ public class Main extends Application {
     public static Stage homeS;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/LogIn.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/MainScene.fxml"));
         homeS = primaryStage;
         primaryStage.setTitle("LogIn Task Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setMaximized(true);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
